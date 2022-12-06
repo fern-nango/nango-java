ae9e85e4d8f01cc89dcdf791e4df382a9e6bbc0a
Deep Singhvi <deep@usebirch.com>
2022-12-06 10:46:03 -0800

    readme and sample app changes (#1)

--- sample-app/src/main/java/sample/App.java
+++ sample-app/src/main/java/sample/App.java
@@ -1,9 +1,27 @@
 package sample;
 
+import com.nango.api.client.sync.endpoints.Add;
+import com.nango.api.client.sync.exceptions.AddException;
+import com.nango.api.client.sync.types.NangoHttpMethod;
+import com.nango.api.client.sync.types.NangoSyncConfig;
 import java.lang.String;
+import com.nango.api.NangoApiClient;
 
 public final class App {
   public static void main(String[] args) {
-    // import com.nango.api.NangoApiClient
+    NangoApiClient nango = new NangoApiClient("https://localhost:3003");
+    try {
+      nango.sync().add(Add.Request.builder()
+          .body(NangoSyncConfig.builder()
+              .method(NangoHttpMethod.GET)
+              .baseUrl("https://www.reddit.com/r/${subreddit}/new.json")
+              .responsePath("data.children")
+              .pagingCursorRequestPath("after")
+              .pagingCursorObjectResponsePath("paging.next.after")
+              .build())
+          .build());
+    } catch (AddException ex) {
+      System.out.println("Failed to add sync: " + ex.getMessage());
+    }
   }
 }
